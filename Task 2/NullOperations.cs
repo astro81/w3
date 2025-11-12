ab7527de10228e43b3e43c7557b995c311b45285
astro81 <caien720@protonmail.com>
2025-11-12 11:54:59 +0545

    task 2

---
+++ Task 2/NullOperations.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace Task_2
+{
+    public class NullOperations
+    {
+        public static void PerformNullChecks()
+        {
+            string? username = null;
+
+            string result = (username == null) ? "Username is not available" : username;
+            Console.WriteLine(result);
+
+            string close = username ?? "Username is not available";
+            Console.WriteLine(close);
+
+            string noClose = null; 
+            noClose ??= "user"; 
+            Console.WriteLine(noClose);
+        }
+    }
+}
+
