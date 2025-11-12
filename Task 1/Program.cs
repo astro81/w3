c6eccac67f79587d95fafafa4f21ec5dc189432f
astro81 <caien720@protonmail.com>
2025-11-12 11:30:05 +0545

    task 1

--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -8,43 +8,13 @@ namespace Task_1
         {
             Console.WriteLine("Hello World");
 
-            add(1, 2);
-            subtract(5, 3);
-            multiply(2, 3);
-            divide(8, 4);
+            Operators.Add(1, 2);
+            Operators.Subtract(5, 3);
+            Operators.Multiply(2, 3);
+            Operators.Divide(8, 4);
 
-            OddEvenFinder(5);
-            OddEvenFinder(8);
-
-        }
-
-        static void add(int a, int b)
-        {
-            Console.WriteLine(a + b);
-        }
-
-        static void subtract(int a, int b)
-        {
-            Console.WriteLine(a - b);
-        }
-
-        static void multiply(int a, int b)
-        {
-            Console.WriteLine(a * b);
-        }
-
-        static void divide(int a, int b)
-        {
-            if (b != 0)
-                Console.WriteLine(a / b);
-            else
-                Console.WriteLine("Cannot divide by zero!");
-        }
-
-        static void OddEvenFinder(int number)
-        {
-            string result = (number % 2 == 0) ? "Even number" : "Odd number";
-            Console.WriteLine(result);
+            Operators.OddEvenFinder(5);
+            Operators.OddEvenFinder(8);
         }
     }
 }
