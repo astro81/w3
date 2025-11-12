f06b25249e5440e102466eb9c391261c04a2ee8b
astro81 <caien720@protonmail.com>
2025-11-12 11:26:34 +0545

    task 1

--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+            add(1, 2);
+            subtract(5, 3);
+            multiply(2, 3);
+            divide(8, 4);
+
+            OddEvenFinder(5);
+            OddEvenFinder(8);
+
+        }
+
+        static void add(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+
+        static void subtract(int a, int b)
+        {
+            Console.WriteLine(a - b);
+        }
+
+        static void multiply(int a, int b)
+        {
+            Console.WriteLine(a * b);
+        }
+
+        static void divide(int a, int b)
+        {
+            if (b != 0)
+                Console.WriteLine(a / b);
+            else
+                Console.WriteLine("Cannot divide by zero!");
+        }
+
+        static void OddEvenFinder(int number)
+        {
+            string result = (number % 2 == 0) ? "Even number" : "Odd number";
+            Console.WriteLine(result);
+        }
+    }
+}
