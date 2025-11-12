ebf567bbadb8b3d347c82fb0c9722a76d98c46bf
astro81 <caien720@protonmail.com>
2025-11-12 12:13:29 +0545

    task 3

---
+++ Task 3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task_3;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter age: ");
+        int? age = Int32.Parse(Console.ReadLine() ?? "0");
+        
+        Console.WriteLine($"age {age}");
+
+        if (age < 13)
+        {
+            Console.WriteLine("Child");
+        } else if (age >= 13 && age <= 19)
+        {
+            Console.WriteLine("Teen");
+        }
+        else if (age >= 20)
+        {
+            Console.WriteLine("Adult");
+        }
+        else
+        {
+            Console.WriteLine("Dead");
+        }
+        
+    }
+}
