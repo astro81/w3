4fc79dcd1f04bd6f2bfbcb9fe6aa3629238d9188
astro81 <caien720@protonmail.com>
2025-11-12 17:14:52 +0545

    task 4

---
+++ Task 4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Task_4;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter Day: ");
+        int? day = Int32.Parse(Console.ReadLine() ?? "1");
+
+        switch (day)
+        {
+            case 1:
+                Console.WriteLine("Sunday");
+                break;
+            case 2:
+                Console.WriteLine("Monday");
+                break;
+            case 3:
+                Console.WriteLine("Tuesday");
+                break;
+            case 4:
+                Console.WriteLine("Wednesday");
+                break;
+            case 5:
+                Console.WriteLine("Thursday");
+                break;
+            case 6:
+                Console.WriteLine("Friday");
+                break;
+            case 7:
+                Console.WriteLine("Saturday");
+                break;
+            default:
+                Console.WriteLine("No day");
+                break;
+                
+        }
+    }
+}
